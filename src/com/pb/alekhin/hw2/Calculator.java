f4fd9ca4c3a3647b6f1de7247b437b9e2ed96aa1
Dmitrij Alekhin <dmitrij.alekhin@privatbank.ua>
2021-10-14 12:57:44 +0300

    init commit

---
+++ src/com/pb/alekhin/hw2/Calculator.java
@@ -0,0 +1,41 @@
+package com.pb.alekhin.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner calc = new Scanner(System.in);
+        int x;
+        int y;
+        String symbol;
+
+        System.out.print(" Укажите первое числовое значение: ");
+
+        x = calc.nextInt();
+
+        System.out.print(" Укажите знак арифметического действия (+,-,*,/): ");
+
+        symbol = calc.next();
+
+        System.out.print(" Укажите второе числовое значение: ");
+
+        y = calc.nextInt();
+
+        switch (symbol) {
+            case "+":
+                System.out.println(x+y);
+                break;
+            case "-":
+                System.out.println(x-y);
+                break;
+            case "*":
+                System.out.println(x*y);
+                break;
+            case "/":
+                if (y == 0) {
+                    System.out.println("Делить на 0 нельзя");
+                }   else {System.out.println(x/y);
+                }
+        }
+    }
+}
